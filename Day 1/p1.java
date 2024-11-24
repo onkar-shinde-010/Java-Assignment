28e1458a2af365a4d9da279a352f2d902ad8072b
Onkar Shinde <onkarshinde2200@gmail.com>
2024-11-24 11:52:10 +0530

    Day One

---
+++ Day 1/p1.java
@@ -0,0 +1,12 @@
+class Demo{
+	public static void main(String[]args){
+		int a=0;
+		if(a>0){
+			System.out.println("Number is positive");
+		}else if(a<0){
+			System.out.println("Number is negative");
+		}else{
+			System.out.println("Number is Zero");
+		}
+	}
+}
